97e085a57a68a64595ca580502d8c3305c9d757b
calcucool <jaypesc@gmail.com>
2025-07-31 06:56:50 -0400

    cleaned up code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import WorldView from "./component/WorldView";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WorldView />
+    </div>
+  );
+}
+
+export default App;
